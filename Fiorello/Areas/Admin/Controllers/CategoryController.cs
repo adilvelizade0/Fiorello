7eec3845c82c22f00241662ea290dd631591a71f
adilvelizade <adilvalizada@outlook.com>
2022-06-06 14:24:32 +0400

    task is updated

--- Fiorello/Areas/Admin/Controllers/CategoryController.cs
+++ Fiorello/Areas/Admin/Controllers/CategoryController.cs
@@ -20,5 +20,56 @@ namespace Fiorello.Areas.Admin.Controllers
             List<Category> categories = _context.Categories.Where(c => !c.IsDeleted).ToList();
             return View(categories);
         }
+        
+        public IActionResult Details(int id)
+        {
+            Category category = _context.Categories.FirstOrDefault(c => !c.IsDeleted && c.Id == id);
+            return View(category);
+        }
+        
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Category category = _context.Categories.FirstOrDefault(c => !c.IsDeleted && c.Id == id);
+            return View(category);
+        }
+        
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            Category existCategory = _context.Categories.FirstOrDefault(c => !c.IsDeleted && c.Id == category.Id);
+            existCategory.Name = category.Name;
+            _context.Categories.Update(existCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        
+         
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            category.IsDeleted = true;
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
     }
 }
